1bf3f5da850448129bf098c96a89676b6653285e
Madhur J. Bajaj <mbajaj.92@gmail.com>
2017-05-13 17:33:28 -0700

    Revert "Updating Server Infrastructure"
    This reverts commit f05408af2560abf3c134651bd93427d5d07d0e6e.

--- src/Server.java
+++ src/Server.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
@@ -11,55 +12,50 @@ import java.net.Socket;
  * It then spawns a thread (instance of the ClientThread class) to handle it. 
  */
 public class Server {
-	public Server(int port) throws IOException {
-		ServerSocket server = new ServerSocket(port); // Creates a listening
-														// port
-		while (true) {
-			Socket client = server.accept(); // Accepts connection request from
-												// client
-			System.out.println("Accepted connection request from " + client.getInetAddress()); // Prints
-
-			// Get the input and output streams for the socket
-			ObjectInputStream in = new ObjectInputStream(client.getInputStream());
-			ObjectOutputStream out = new ObjectOutputStream(client.getOutputStream());
-
-			// The server asks the client the email id
-			String requestUsername = "Please enter your email id connected with the TIPPERs system";
-			out.writeObject(requestUsername);
-			String userEmail = "";
-			String password = "";
-
-			try {
-				// Reads the userEmail from the socket
-				userEmail = (String) in.readObject();
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-			}
-
-			// TODO : Should check from the database that the user email is a
-			// registered one
-
-			// Asks client for password
-			String requestPassword = "Please enter your password";
-			out.writeObject(requestPassword);
-
-			try {
-				// Reads the Password from the socket
-				password = (String) in.readObject();
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-			}
-
-			// TODO : Check that the user id and passwords match (Database)
-
-			// Control should reach here only if all conditions satisfied
-
-			// Spawn a thread to check for pending requests
-			/* No need to spawn a new thread specifically, I believe we can
-			 * simply run a small search here; and then spawn a Probe threa */
-			ClientThread clientThread = new ClientThread(client, userEmail);
-
-			// TODO : figure out when to shut the server
-		}
-	}
+    public Server(int port) throws IOException{
+        ServerSocket server = new ServerSocket(port);   // Creates a listening port
+        while (true) {
+            Socket client = server.accept();    // Accepts connection request from client
+            System.out.println("Accepted connection request from " + client.getInetAddress());  // Prints client address
+            
+            // Get the input and output streams for the socket
+            ObjectInputStream in = new ObjectInputStream(client.getInputStream());
+            ObjectOutputStream out = new ObjectOutputStream(client.getOutputStream());
+            
+            // The server asks the client the email id
+            String requestUsername = "Please enter your email id connected with the TIPPERs system";
+            out.writeObject(requestUsername);
+            String userEmail = "";
+            String password = "";
+            
+            try {
+                // Reads the userEmail from the socket
+                userEmail = (String)in.readObject();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+            
+            // TODO : Should check from the database that the user email is a registered one 
+         
+            // Asks client for password
+            String requestPassword = "Please enter your password";
+            out.writeObject(requestPassword);
+            
+            try {
+                // Reads the Password from the socket
+                password = (String)in.readObject();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+            
+            // TODO : Check that the user id and passwords match (Database)
+            
+            //  Control should reach here only if all conditions satisfied
+            
+            // Spawn a thread to check for pending requests
+            ClientThread clientThread = new ClientThread(client, userEmail);
+            
+            // TODO : figure out when to shut the server
+        }
+    }
 }
