ebe5a920cd4cf34cda3a2cd3691fc31970fe61fb
Madhur J. Bajaj <mbajaj.92@gmail.com>
2017-05-13 18:22:39 -0700

    Adding TestClient and TestServer for Java Socket Communication
    Only String Passing works right now; but I am sure, we can send and
    receive Serialized Objects as well.
    lets see.

---
+++ src/Test/TestSocketClient.java
@@ -0,0 +1,34 @@
+package Test;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+//File Name GreetingClient.java
+import java.net.Socket;
+
+public class TestSocketClient {
+
+public static void main(String [] args) {
+   String serverName = "10.11.228.111";//args[0];
+   int port = 6066;//nteger.parseInt(args[1]);
+   try {
+      System.out.println("Connecting to " + serverName + " on port " + port);
+      Socket client = new Socket(serverName, port);
+      
+      System.out.println("Just connected to " + client.getRemoteSocketAddress());
+      OutputStream outToServer = client.getOutputStream();
+      DataOutputStream out = new DataOutputStream(outToServer);
+      
+      out.writeUTF("Hello from " + client.getLocalSocketAddress());
+      InputStream inFromServer = client.getInputStream();
+      DataInputStream in = new DataInputStream(inFromServer);
+      
+      System.out.println("Server says " + in.readUTF());
+      client.close();
+   }catch(IOException e) {
+      e.printStackTrace();
+   }
+}
+}
