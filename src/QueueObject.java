bce3528f475139a26ec72fd3bcdd0d2e6d50da0b
Harshini <harshini307@gmail.com>
2017-05-14 00:49:28 -0700

    Modified QueueObject to add a count of probe thread pings

--- src/QueueObject.java
+++ src/QueueObject.java
@@ -2,15 +2,19 @@
  * Each user has a queue of pending messages that have to be delivered - each object in the queue
  * is an instance of this class. It contains the ID associated with the message (using which the contents
  * of the message can be retrieved from the database - this is only done if the location constraint is 
- * satisfied) and the location (which is checked and compared with the user's current location). 
+ * satisfied), a counter of the number of times the object has been pinged by the Probe Thread (initially 
+ * set to 100 and decremented with each ping) and the location (which is checked and compared with the 
+ * user's current location). 
  */
 public class QueueObject {
     private int messageID;
     private Location location;
+    private int probePings;
     
     public QueueObject(int messageID, Location location) {
         this.messageID = messageID;
         this.location = location;
+        this.probePings = 100;
     }
     
     public int getMessageID() {
@@ -19,5 +23,9 @@ public class QueueObject {
     
     public Location getLocation() {
         return location;
+    }
+    
+    public int getProbePings() {
+        return probePings;
     }
 }
