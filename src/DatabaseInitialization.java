399581088550696b524e0d51364baafdd0561572
Harshini <harshini307@gmail.com>
2017-05-16 13:21:25 -0700

    Transaction Table Initialization and DB cleanup added

--- src/DatabaseInitialization.java
+++ src/DatabaseInitialization.java
@@ -9,15 +9,14 @@ import java.io.IOException;
 import java.sql.*;
 
 public class DatabaseInitialization {
-    private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
-    private static final String DB_URL = "jdbc:mysql://localhost/";
+    protected static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+    protected static final String DB_URL = "jdbc:mysql://localhost/";
     
-    private static final String USER = "harshini";
-    private static final String PASS = "password";
+    protected static final String USER = "harshini";
+    protected static final String PASS = "password";
     
-    private static final String DB_NAME = "MESSENGER";
-    private static final String TABLES_URL = "jdbc:mysql://localhost/" + DB_NAME;
-
+    protected static final String DB_NAME = "MESSENGER";
+    protected static final String TABLES_URL = "jdbc:mysql://localhost/" + DB_NAME;
     
     /*
      * Creates a database called "MESSENGER". 
@@ -71,18 +70,18 @@ public class DatabaseInitialization {
         
         try {
             Class.forName(JDBC_DRIVER);
-            System.out.println("Connecting to a selected database...");
+            System.out.println("Connecting to the messenger database...");
             conn = DriverManager.getConnection(TABLES_URL, USER, PASS);
             System.out.println("Connected database successfully...");
             
-            System.out.println("Creating table in given database...");
+            System.out.println("Creating users table in given database...");
             stmt = conn.createStatement();
             
             String createTable = "CREATE TABLE USERS " +
                          "(id INTEGER not NULL, " +
                          " UserEmail VARCHAR(255), " + 
                          " Password VARCHAR(255), " + 
-                         " PRIMARY KEY ( id ))"; 
+                         " PRIMARY KEY (id))"; 
 
             stmt.executeUpdate(createTable);
             System.out.println("Created table in given database...");
@@ -141,9 +140,102 @@ public class DatabaseInitialization {
     
     /*
      * Creates a table "TRANSACTIONS" which stores the record of all the pending messages. 
-     * For each transaction, it stores the sender, receiver, messageId, messageText
+     * For each transaction, it stores the sender, receiver, messageId, messageText.
+     * 
+     * TODO : For now, just added support for simple textual messages that can have a 
+     * maximum of 256 characters. Make this more generic, should be able to add pictures,
+     * and increase the count of characters.
+     * 
+     * TODO : For now, assumes that the location is a string representing a room number. 
+     * Later, should be able to directly store an instance of the type Location in the 
+     * database.
      */
-    public static void createTransactionTable() {
+    public static void createTransactionsTable() {
+        Connection conn = null;
+        Statement stmt = null;
         
+        try {
+            Class.forName(JDBC_DRIVER);
+            System.out.println("Connecting to the messenger database...");
+            conn = DriverManager.getConnection(TABLES_URL, USER, PASS);
+            System.out.println("Connected database successfully...");
+            
+            System.out.println("Creating transactions table in given database...");
+            stmt = conn.createStatement();
+            
+            String createTable = "CREATE TABLE TRANSACTIONS " +
+                         "(id INTEGER not NULL, " +
+                         " SenderEmail VARCHAR(255), " + 
+                         " ReceiverEmail VARCHAR(255), " + 
+                         " MessageText VARCHAR(255), " +
+                         " RoomNumber VARCHAR(255), " +
+                         " PRIMARY KEY (id))"; 
+
+            stmt.executeUpdate(createTable);
+            System.out.println("Created table in given database...");
+         } catch (SQLException se) {
+            se.printStackTrace();
+         } catch (Exception e) {
+            e.printStackTrace();
+         } finally {
+            try {
+               if (stmt != null) {
+                   conn.close();
+               }
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+            
+            try {
+               if (conn != null) {
+                   conn.close();
+               }
+            } catch (SQLException se) {
+               se.printStackTrace();
+            }
+         }
+    }
+    
+    /* Deletes the database and the tables from the system so that when the program is run
+     * again, everything works.
+     */
+    public static void cleanup() {
+        Connection conn = null;
+        Statement stmt = null;
+        
+        try {
+           Class.forName(JDBC_DRIVER);
+
+           System.out.println("Connecting to a selected database...");
+           conn = DriverManager.getConnection(DB_URL, USER, PASS);
+           System.out.println("Connected database successfully...");
+           
+           System.out.println("Deleting database...");
+           stmt = conn.createStatement();
+           
+           String sql = "DROP DATABASE MESSENGER";
+           stmt.executeUpdate(sql);
+           System.out.println("Database deleted successfully...");
+        } catch (SQLException se) {
+           se.printStackTrace();
+        } catch (Exception e) {
+           e.printStackTrace();
+        } finally {
+           try {
+              if (stmt != null) {
+                  conn.close();
+              }
+           } catch (SQLException se) {
+               se.printStackTrace();
+           }
+           
+           try {
+              if (conn != null) {
+                  conn.close();
+              }
+           } catch (SQLException se) {
+              se.printStackTrace();
+           }
+        }
     }
 }
