ee5aecb29b0ea7a4aa019408d36b6757f85d5c10
Madhur J. Bajaj <mbajaj.92@gmail.com>
2017-05-29 18:41:15 -0700

    Fixing Client

--- src/ClientCode/Client.java
+++ src/ClientCode/Client.java
@@ -15,8 +15,8 @@ public class Client {
 	private Socket test_socket;
 	private ObjectInputStream inputStream = null;
 	private ObjectOutputStream outputStream = null;
-	ClientReceive clientReceive;
-	Scanner scanner;
+	private ClientReceive clientReceive;
+	private Scanner scanner;
 
 	public static void main(String args[]) {
 		new Client();
@@ -31,7 +31,6 @@ public class Client {
 			String user_id = scanner.nextLine();
 
 			System.out.println("Enter the Password");
-			// Scanner scanner = new Scanner(System.in);
 			String password = scanner.nextLine();
 
 			Message msg = new Message();
@@ -43,7 +42,7 @@ public class Client {
 			inputStream = new ObjectInputStream(test_socket.getInputStream());
 			outputStream.writeObject(msg);
 			outputStream.flush();
-			
+
 			Message fromServer = (Message) inputStream.readObject();
 			System.out.println(fromServer);
 			if ((fromServer.msgType == Message.MsgType.LOGIN_MSG) && ((fromServer.field1.compareTo(user_id) == 0))
@@ -60,65 +59,66 @@ public class Client {
 				System.out.println("FROM = " + fromServer.field4);
 			}
 
-			// Receive a msg from client to confirm registered user and then
-			// proceed
+			/*
+			 * Receive a msg from client to confirm registered user and then
+			 * proceed
+			 */
 			clientReceive = new ClientReceive(user_id);
-			// test_socket.close();
 			clientReceive.start();
-			// System.out.println("Your user id is: " + user_id);
 
 			System.out.println("Enter your choice 1. Send a message 2. Logout");
 			int choice = scanner.nextInt();
-			String slash_n = scanner.nextLine();
-			// System.out.println("Your choice is :" + choice);
-			// int choice = 1;
+			scanner.nextLine();
+
 			while (choice != 2) {
 				System.out.println("Enter the recipient user_id");
 				String recipient_id = scanner.nextLine();
-				// System.out.println("Recipient user_id is "+ recipient_id);
+
 				System.out.println("Enter your message");
 				String text_msg = scanner.nextLine();
 				System.out.println("Enter the location");
 				String location = scanner.nextLine();
 
-				System.out.println("Your user id is:  " + recipient_id);
-				System.out.println("Your msg is :  " + text_msg);
-				System.out.println("Your location is :  " + location);
-				// test_socket = new Socket(servername,port);
+				System.out.println("You are sending the message to:  " + recipient_id);
+				System.out.println("The message is :  " + text_msg);
+				System.out.println("The Location is :  " + location);
+
 				msg.msgType = MsgType.SEND_MSG;
 				msg.field1 = user_id;
 				msg.field2 = recipient_id;
 				msg.field3 = text_msg;
-				msg.field4 = location;// Convert it to location object
+				msg.field4 = location;
+
 				test_socket = new Socket(servername, port);
 				outputStream = new ObjectOutputStream(test_socket.getOutputStream());
+				inputStream = new ObjectInputStream(test_socket.getInputStream());
 				outputStream.writeObject(msg);
 				outputStream.flush();
-				// test_socket.close();
 
 				System.out.println("Enter your choice 1. Send a message 2. Logout ");
 				choice = scanner.nextInt();
-				slash_n = scanner.nextLine();
-
+				scanner.nextLine();
 			}
+
 			msg = new Message();
 			msg.msgType = MsgType.LOGOFF_MSG;
 			msg.field1 = user_id;
+
 			test_socket = new Socket(servername, port);
-			inputStream = new ObjectInputStream(test_socket.getInputStream());
 			outputStream = new ObjectOutputStream(test_socket.getOutputStream());
+			inputStream = new ObjectInputStream(test_socket.getInputStream());
 			outputStream.writeObject(msg);
 			outputStream.flush();
+
+			/* Wait for the server to ACK and ONLY then die */
 			msg = (Message) inputStream.readObject();
 			System.out.println("Thanks for your Interaction ");
-			// clientreceive.stop // Kill the thread here
+
 		} catch (SocketException se) {
 			se.printStackTrace();
-			// System.exit(0);
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} finally {
 			clientReceive.down();
